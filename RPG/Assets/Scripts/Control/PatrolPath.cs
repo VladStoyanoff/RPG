09927902d6746f3ad4d3bf4b8e5057eb5fd550cf
VladStoyanoff <vladstoyanoff@gmail.com>
2022-08-26 20:06:31 +0100

    visualising waypoints

---
+++ RPG/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control 
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawSphere(transform.GetChild(i).position, .5f);
+            }
+        }
+    }
+}
