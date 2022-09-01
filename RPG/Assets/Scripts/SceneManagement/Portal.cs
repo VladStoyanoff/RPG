c40847693af75183879c4b694796ea24bf9d5db7
VladStoyanoff <vladstoyanoff@gmail.com>
2022-09-01 19:05:05 +0100

    portals + warp

--- RPG/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,16 +1,47 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour 
     {
+        public static Portal Instance { get; private set; }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+
         void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player" == false) return;
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(Transition());
+        }
+
+        IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            var otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            Destroy(gameObject);
+        }
+
+        void UpdatePlayer(Portal otherPortal)
+        {
+            var player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                return portal;
+            }
+            return null;
         }
     }
-
 }
