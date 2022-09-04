95a8fac07a349a56a2858b4dad637190c0468962
VladStoyanoff <vladstoyanoff@gmail.com>
2022-09-04 15:52:12 +0100

    binary formatter for serialization

--- RPG/Assets/Scripts/---/SavingSystemX.cs
+++ RPG/Assets/Scripts/---/SavingSystemX.cs
@@ -1,35 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace RPG.lol 
 {
     public class SavingSystemX : MonoBehaviour
     {
+
         public void Save(string saveFile)
         {
             var path = GetPathFromSaveFile(saveFile);
             Debug.Log("Saving to " + path);
             using (var stream = File.Open(path, FileMode.Create))
             {
-
+                var formatter = new BinaryFormatter();
+                var position = new SerializableVector3X(GetPlayerTransform().position);
+                formatter.Serialize(stream, position);
             }
         }
 
         public void Load(string saveFile)
-        {
+        { 
             var path = GetPathFromSaveFile(saveFile);
             Debug.Log("Loading from " + path);
             using (var stream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-                Debug.Log(Encoding.UTF8.GetString(buffer));
+                var formatter = new BinaryFormatter();
+                var position = (SerializableVector3X)formatter.Deserialize(stream);
+                GetPlayerTransform().position = position.ToVector();
             }
         }
 
+        byte[] SerializeVector(Vector3 vector)
+        {
+            // a float takes up 4 bytes
+            var vectorBytes = new byte[3 * 4];
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+            return vectorBytes;
+        }
+
+        Vector3 DeserializeVector(byte[] buffer)
+        {
+            var deserializedVector = new Vector3();
+            deserializedVector.x = BitConverter.ToSingle(buffer, 0);
+            deserializedVector.y = BitConverter.ToSingle(buffer, 4);
+            deserializedVector.z = BitConverter.ToSingle(buffer, 8);
+            return deserializedVector;
+        }
+
+        Transform GetPlayerTransform() => GameObject.FindWithTag("Player").transform;
         string GetPathFromSaveFile(string saveFile) => Path.Combine(Application.persistentDataPath, saveFile + ".sav");
     }
 }
