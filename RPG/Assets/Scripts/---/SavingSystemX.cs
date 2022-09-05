635e2a3c4b8ba9fc8a14dccdf4756be4cecbf29f
VladStoyanoff <vladstoyanoff@gmail.com>
2022-09-05 12:02:05 +0100

    saving multiple scenes part 1

--- RPG/Assets/Scripts/---/SavingSystemX.cs
+++ RPG/Assets/Scripts/---/SavingSystemX.cs
@@ -9,47 +9,57 @@ namespace RPG.lol
 {
     public class SavingSystemX : MonoBehaviour
     {
-
+        #region SAVING
         public void Save(string saveFile)
         {
+            SaveFile(saveFile, CaptureState());
+        }
+
+        void SaveFile(string saveFile, Dictionary<string, object> state)
+        {
             var path = GetPathFromSaveFile(saveFile);
-            Debug.Log("Saving to " + path);
             using (var stream = File.Open(path, FileMode.Create))
             {
                 var formatter = new BinaryFormatter();
-                formatter.Serialize(stream, CaptureState());
+                formatter.Serialize(stream, state);
             }
         }
 
-        public void Load(string saveFile)
-        { 
-            var path = GetPathFromSaveFile(saveFile);
-            Debug.Log("Loading from " + path);
-            using (var stream = File.Open(path, FileMode.Open))
-            {
-                var formatter = new BinaryFormatter();
-                RestoreState(formatter.Deserialize(stream));
-            }
-        }
-
-        object CaptureState()
+        Dictionary<string, object> CaptureState()
         {
             Dictionary<string, object> stateDict = new Dictionary<string, object>();
-            foreach(var saveableEntity in FindObjectsOfType<SaveableEntityX>())
+            foreach (var saveableEntity in FindObjectsOfType<SaveableEntityX>())
             {
                 stateDict[saveableEntity.GetUniqueIdentifier()] = saveableEntity.CaptureState();
             }
             return stateDict;
         }
+        #endregion
 
-        void RestoreState(object state)
+        #region LOADING
+        public void Load(string saveFile)
         {
-            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+            RestoreState(LoadFile(saveFile));
+        }
+
+        void RestoreState(Dictionary<string, object> state)
+        {
             foreach (var saveableEntity in FindObjectsOfType<SaveableEntityX>())
             {
-                saveableEntity.RestoreState(stateDict[saveableEntity.GetUniqueIdentifier()]);
+                saveableEntity.RestoreState(state[saveableEntity.GetUniqueIdentifier()]);
             }
         }
+
+        Dictionary<string, object> LoadFile(string saveFile)
+        {
+            var path = GetPathFromSaveFile(saveFile);
+            using (var stream = File.Open(path, FileMode.Open))
+            {
+                var formatter = new BinaryFormatter();
+                return (Dictionary<string, object>)formatter.Deserialize(stream);
+            }
+        }
+        #endregion
 
         string GetPathFromSaveFile(string saveFile) => Path.Combine(Application.persistentDataPath, saveFile + ".sav");
     }
